90e21954bfa273e3e17ba2b8afc4ca8a04faf251
Qbisst <ismaelaperez@outlook.com>
2019-06-02 21:00:53 -0400

    redoing server.js file

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-var PORT = process.env.PORT || 3000;
 // Initialize Express
 var app = express();
+
+var PORT = process.env.PORT || 3000;
+
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/polygonNews";
 
@@ -21,7 +23,6 @@ app.use(express.static("public"));
 
 //setup handlebars//
 var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
